05cb47779a10b83d3f18b48412e33a54500995a2
WillyLi <wanttobe0618@gmail.com>
2016-08-05 18:29:17 +0800

    0806

--- contact/email.php
+++ contact/email.php
@@ -1,8 +1,8 @@
 <?php
 if($_POST){
-    $name = $_POST['form_name'];
-    $email = $_POST['form_email'];
-    $message = $_POST['form_msg'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
 //send email
     mail("wanttobe0618@gmail.com", "51 Deep comment from" .$email, $message);
